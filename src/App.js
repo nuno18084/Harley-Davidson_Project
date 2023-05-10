565b03316602d3469ea4f7b9d3403b1a0e9dbead
Nuno Colaço <nunocolaco@Nunos-MacBook-Pro.local>
2023-05-10 14:22:59 +0100

    Start creating the navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-section">
+      <div className="navbar-container">
+        <img
+          className="logo"
+          src="https://www.imgtr.ee/images/2023/05/10/lJVam.png"
+          alt="logo"
+          height={45}
+          width={200}
+        />
+        <div className="center-nav">
+          <div>Bikes</div>
+          <div>New Arrivals</div>
+          <div>Parts</div>
+          <div>Shop</div>
+          <div>Services</div>
+        </div>
+        <button className="navbar-button">Contact Us</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
