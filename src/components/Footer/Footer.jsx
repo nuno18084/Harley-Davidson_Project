8b60a35cdead2aead449b837ce9cc052dd03df8c
Nuno Colaço <nunocolaco@Nunos-MacBook-Pro.local>
2023-05-21 19:40:07 +0100

    Add the footer without styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,94 @@
 import React from "react";
+import Logo from "../../images/logo.png";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiFillTwitterSquare } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import { BiFileFind } from "react-icons/bi";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const handleDealer = () => {
+    window.open(
+      "https://www.harley-davidson.com/pt/pt/tools/find-a-dealer.html",
+      "_blank"
+    );
+  };
+
+  return (
+    <div className="footer-main-container">
+      <div className="footer-first-container">
+        <div className="footer-logo-section">
+          <div>
+            <img src={Logo} alt="footer-logo" height={35} width={150} />
+          </div>
+          <div>
+            <h5>Stay Connected with us</h5>
+          </div>
+          <div className="footer-social-media-section">
+            <div>
+              <AiFillFacebook />
+            </div>
+            <div>
+              <AiFillTwitterSquare />
+            </div>
+            <div>
+              <AiFillLinkedin />
+            </div>
+          </div>
+        </div>
+        <div className="footer-nav-section">
+          <div>
+            <h5>Home</h5>
+            <ul>
+              <li>About Us</li>
+              <li>Events</li>
+              <li>News</li>
+              <li>Blog</li>
+            </ul>
+          </div>
+          <div>
+            <h5>About</h5>
+            <ul>
+              <li>Company</li>
+              <li>Career</li>
+              <li>Investors</li>
+              <li>News</li>
+            </ul>
+          </div>
+          <div>
+            <h5>Bikes</h5>
+            <ul>
+              <li>Motorcycles</li>
+              <li>Collection</li>
+              <li>Electronic</li>
+              <li>Shop all</li>
+            </ul>
+          </div>
+          <div>
+            <h5>Offers</h5>
+            <ul>
+              <li>Shipping</li>
+              <li>Exchanges</li>
+              <li>Insurance</li>
+              <li>Service</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="footer-second-container">
+        <div>
+          <h5>Privacy Policy</h5>
+          <h5>Disclaimers</h5>
+          <h5>Insurance</h5>
+        </div>
+        <div>
+          <button onClick={handleDealer}>
+            <BiFileFind size={20} />
+            Find a Dealer
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
